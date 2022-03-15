b7e222868973116fd3cfe8dd39e0a894da0ac405
GoTrust-TrungNT <trung.nguyen@gotrust.vn>
2022-03-15 19:11:00 +0700

    Add log to debug

--- install_package.sh
+++ install_package.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+username="dev1"
+password="abc123"
 
+echo "---> Install package openssh-server and sudo"
 apt update && apt install -y openssh-server sudo
+echo "---> service ssh start"
 service ssh start
+echo "---> Prepare executing add user"
 chmod +x add_user_passwd.sh
-./add_user_passwd.sh dev1 abc123
-usermod -aG sudo dev1
+./add_user_passwd.sh $username $password
+echo "---> Add user done"
+usermod -aG sudo $username
+echo "---> Escalate user!"
 
